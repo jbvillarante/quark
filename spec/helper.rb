8b0dc5cc4096f376d5ac0391f15d9fedc14994bc
Anna Marseille D. Gabutero <agabutero@friendster.com>
2010-11-26 15:37:58 +0800

    Removing unnecessary code

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'bundler'
 begin
   Bundler.setup(:default, :development)
@@ -7,12 +6,10 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'test/unit'
+
 require 'rspec'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'quark'
 
-class Test::Unit::TestCase
-end
+require 'quark'
