a5bb5031744f77006dc89d301d83700f16006340
Anna Marseille D. Gabutero <agabutero@friendster.com>
2010-12-03 16:15:14 +0800

    Tests for session creation through Quark::Client

--- spec/quark/client_spec.rb
+++ spec/quark/client_spec.rb
@@ -5,12 +5,6 @@ describe 'Quark::Client' do
     @api_key = '718fb34497589503915e85470d9d5511'
     @api_secret = 'b39c091ea8bb895345f652cc3217a1cf'
     @default_endpoint = 'http://api.friendster.com/v1'
-
-    stub_data = <<-XML
-    <?xml version="1.0" encoding="UTF-8" ?><token_response xmlns="http://api.friendster.com/v1/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://api.friendster.com/v1/ http://api.friendster.com/v1/friendster.xsd" ><auth_token>74cef776c6338d8.40566214</auth_token></token_response>
-    XML
-    stub_response = Typhoeus::Response.new(:code => 200, :headers => "", :body => stub_data)
-    Typhoeus::Hydra.hydra.stub(:post, "#{@default_endpoint}/token").and_return(stub_response)
   end
 
   specify 'should require setting the API key' do
@@ -23,12 +17,42 @@ describe 'Quark::Client' do
 
   specify 'should allow setting the API endpoint' do
     endpoint = 'http://localhost/v1'
-    session = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret, :endpoint => endpoint)
-    session.endpoint.should == endpoint
+    client = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret, :endpoint => endpoint)
+    client.endpoint.should == endpoint
   end
 
   specify "should default to Friendster's v1 API endpoint" do
-    session = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret)
-    session.endpoint.should == @default_endpoint
+    client = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret)
+    client.endpoint.should == @default_endpoint
+  end
+
+  specify 'should be able to obtain an auth_token' do
+    stub_response = Typhoeus::Response.new(:code => 200, :headers => "", :body => xml_test_data('token_response_valid'))
+    Typhoeus::Hydra.hydra.stub(:post, %r{/token}).and_return(stub_response)
+
+    client = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret)
+    client.get_token.should == '74cef776c6338d8.40566214'
+  end
+
+  specify 'can log in with a user email and password using a trusted API key' do
+    pending 'unimplemented'
+  end
+
+  specify 'can accept a session key and UID directly' do
+    session_key = 'foo'
+    uid = 18236912940
+    client = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret)
+    session = client.create_session(:session_key => session_key, :uid => uid)
+    session.session_key.should == session_key
+    session.uid.should == uid
+  end
+
+  specify 'can obtain a session key and UID from an authenticated auth_token' do
+    stub_response = Typhoeus::Response.new(:code => 200, :headers => "", :body => xml_test_data('session_response_valid'))
+    Typhoeus::Hydra.hydra.stub(:post, %r{/session}).and_return(stub_response)
+    client = Quark::Client.new(:api_key => @api_key, :api_secret => @api_secret)
+    session = client.create_session_from_token(@auth_token)
+    session.session_key.should == 'Peni5ks8UkrpLayjuhpXy53EoiyCZ0zG-43169473'
+    session.uid.should == '43169473'
   end
 end
