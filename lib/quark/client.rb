5e69978414014a66a640f7a2c7209b6c77a219f1
Anna Marseille D. Gabutero <agabutero@friendster.com>
2010-12-03 16:15:41 +0800

    Session creation code

--- lib/quark/client.rb
+++ lib/quark/client.rb
@@ -25,5 +25,16 @@ module Quark
       response = Quark::UnsignedRequest.post(@settings[:endpoint], 'token', :params => { :api_key => @settings[:api_key] })
       Nokogiri::XML(response.body).css("auth_token").text
     end
+
+    def create_session_from_token(token)
+      response = Quark::SignedRequest.post(@settings[:endpoint], 'session', @settings[:api_secret ], :params => { :api_key => @settings[:api_key], :auth_token => token })
+      session_key = Nokogiri::XML(response.body).css('session_key').text
+      uid = Nokogiri::XML(response.body).css('uid').text
+      create_session(:session_key => session_key, :uid => uid)
+    end
+
+    def create_session(params)
+      Quark::Session.new(:api_key => @settings[:api_key], :api_secret => @settings[:api_secret], :endpoint => @settings[:endpoint], :session_key => params[:session_key], :uid => params[:uid])
+    end
   end
 end
