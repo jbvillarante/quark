879b99859e528820a5cf2e87cc7bd79b4492c80a
Anna Marseille D. Gabutero <agabutero@friendster.com>
2010-12-13 09:19:57 +0800

    Removing default nil key values

--- lib/quark/session.rb
+++ lib/quark/session.rb
@@ -4,10 +4,7 @@ module Quark
   class Session
     def initialize(params)
       @settings = {
-        :endpoint => 'http://api.friendster.com/v1',
-        :api_key => nil,
-        :api_secret => nil,
-        :session_key => nil,
+        :endpoint => 'http://api.friendster.com/v1'
       }.merge(params)
 
       [ :api_key, :api_secret, :session_key, :uid ].each do |required|
